ff7992c70613b84478e90bf87e6c4c941a976da2
R. K. <ratna.kumar62@gmail.com>
2025-04-14 14:09:29 +0000

    Update 2 files
    - /.gitlab-ci.yml
    - /dags/beta_test.py

--- dags/beta_test.py
+++ dags/beta_test.py
@@ -3,9 +3,11 @@ from datetime import datetime, timedelta
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from airflow.operators.python import PythonOperator, BranchPythonOperator, ShortCircuitOperator
 from airflow.models import Variable
-from airflow.providers.google.cloud.hooks.bigquery import BigQueryhook
+from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
 from airflow.operators.empty import EmptyOperator
 from airflow.utils.task_group import TaskGroup
+from io import StringIO
 import pandas as pd
 
 
@@ -22,106 +24,106 @@ default_args = {
     'retry_delay': timedelta(minutes=2), # if we need to retry how long should we wait before retrying?
 }
 
-dag = DAG('beta_move_dag',
+with DAG('beta_move_dag_test',
            schedule_interval=None,
           default_args=default_args,
           catchup=False,
           template_searchpath=['./sqls']
-          )
+          ) as dag :
 
 
-# proclist = {"name":"Data_load_stg", "dataset":"BETA_STG"}
-file = 'proc_list.csv'
+    # proclist = {"name":"Data_load_stg", "dataset":"BETA_STG"}
+    file = 'proc_list.csv'
 
 
-def gcs_to_df():
-    gcshook = GCSHook(gcp_conn_id=GCP_CONN_ID)
-    gcs_file_path = f'proc_list.csv'
-    file_bytes = gcshook.download(object_name=gcs_file_path)
-    file = file_bytes.decode('utf-8')
-    df = pd.read_csv(StringIO(file))
-    return df
+    def gcs_to_df():
+        gcshook = GCSHook(gcp_conn_id=GCP_CONN_ID)
+        gcs_file_path = f'dags/proc_list.csv'
+        file_bytes = gcshook.download(bucket_name="us-central1-tests-3b5f7783-bucket",object_name=gcs_file_path)
+        file = file_bytes.decode('utf-8')
+        df = pd.read_csv(StringIO(file))
+        return df
 
-#proclist = pd.read_csv(file,escapechar="\\", dtype=str)
+    #proclist = pd.read_csv(file,escapechar="\\", dtype=str)
 
-proclist=gcs_to_df()
+    proclist=gcs_to_df()
 
-start = EmptyOperator(
-    task_id="Start"
-)
+    start = EmptyOperator(
+        task_id="Start"
+    )
 
-end = EmptyOperator(
-    task_id="End"
-)
+    end = EmptyOperator(
+        task_id="End"
+    )
 
-def check_procedure(**kwargs):
-    proc_name = kwargs["name"]
-    dataset = kwargs["dataset"]
-    conditional_true_task = ["conditional_true_task"]
-    conditional_false_task = ["conditional_false_task"]
+    def check_procedure(**kwargs):
+        proc_name = kwargs["name"]
+        dataset = kwargs["dataset"]
+        conditional_true_task = ["conditional_true_task"]
+        conditional_false_task = ["conditional_false_task"]
 
-    bq_hook = BigQueryhook(gcp_conn_id=GCP_CONN_ID, use_legacy_sql=False)
+        bq_hook = BigQueryHook(gcp_conn_id=GCP_CONN_ID, use_legacy_sql=False)
 
-    query = f"select count(1) as count from {dataset}.INFORMATION_SCHEMA.ROUTINES where routine = '{proc_name}'"
+        query = f"select count(1) as count from {dataset}.INFORMATION_SCHEMA.ROUTINES where routine = '{proc_name}'"
 
-    bq_df = bq_hook(sql=query)
+        bq_df = bq_hook(sql=query)
 
-    exists = df['count'][0] > 0
+        exists = df['count'][0] > 0
 
-    return conditional_true_task if exists else conditional_false_task
+        return conditional_true_task if exists else conditional_false_task
 
 
-previous = None
+    previous = None
 
-for procedure in proclist.itertuples():
-    procedure_name = procedure["procedure_name"]
-    dataset_name = procedure["dataset"]
+    for procedure in proclist.itertuples():
+        procedure_name = procedure.procedure_name
+        dataset_name = procedure.dataset
 
-    with TaskGroup(group_id=f'{dataset_name}') as executegroup:
+        with TaskGroup(group_id=f'{dataset_name}') as executegroup:
 
-        check_procedure_exists = BranchPythonOperator(
-        task_id='check_procedure',
-        python_callable=check_procedure,
-        op_kwargs={
-            "name":procedure_name,
-            "dataset":dataset_name,
-            "conditional_true_task":f"call_{procedure_name}",
-            "conditional_false_task":f"create_{procedure_name}"
-            }
-        )
-
-        createprocedure = BigQueryInsertJobOperator(
-            task_id=f"create_{procedure_name}",
-            configuration={
-                "query": {
-                    "query": "{% include '" + procedure_name + ".sql' %}",
-                    "useLegacySql": False,
+            check_procedure_exists = BranchPythonOperator(
+            task_id='check_procedure',
+            python_callable=check_procedure,
+            op_kwargs={
+                "name":procedure_name,
+                "dataset":dataset_name,
+                "conditional_true_task":f"call_{procedure_name}",
+                "conditional_false_task":f"create_{procedure_name}"
                 }
-            },
-            location='US',
-        )
+            )
 
-        call_procedure = BigQueryInsertJobOperator(
-            task_id="call_{procedure_name}",
-            configuration={
-                "query": {
-                    "query": f"CALL {dataset_name}.{procedure_name}() ; ",
-                    "useLegacySql": False,
-                }
-            },
-            location='US',
-        )
+            createprocedure = BigQueryInsertJobOperator(
+                task_id=f"create_{procedure_name}",
+                configuration={
+                    "query": {
+                        "query": "{% include '" + procedure_name + ".sql' %}",
+                        "useLegacySql": False,
+                    }
+                },
+                location='US',
+            )
 
-        check_procedure_exists >> [createprocedure, call_procedure]
-        createprocedure >> call_procedure
-        call_procedure >> end
-    
-    if previous:
-        previous >> executegroup
-    previous = executegroup
+            call_procedure = BigQueryInsertJobOperator(
+                task_id="call_{procedure_name}",
+                configuration={
+                    "query": {
+                        "query": f"CALL {dataset_name}.{procedure_name}() ; ",
+                        "useLegacySql": False,
+                    }
+                },
+                location='US',
+            )
 
-    
-start >> executegroup >> end
+            check_procedure_exists >> [createprocedure, call_procedure]
+            createprocedure >> call_procedure
+            call_procedure >> end
+        
+        if previous:
+            previous >> executegroup
+        previous = executegroup
+
+        
+    start >> executegroup >> end
 
 
 
